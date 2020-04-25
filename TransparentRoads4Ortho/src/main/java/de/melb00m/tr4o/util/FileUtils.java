ac9aa939e70a45efbc639c0aee28cc0912f41823
Martin Buchheim <martin.buchheim@ikor.de>
2020-04-25 22:15:52 +0200

    * Improved logging mechanism and binding to verbose-args * Added DSFTool version check

--- TransparentRoads4Ortho/src/main/java/de/melb00m/tr4o/util/FileUtils.java
+++ TransparentRoads4Ortho/src/main/java/de/melb00m/tr4o/util/FileUtils.java
@@ -5,14 +5,20 @@ import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.file.FileVisitOption;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 public class FileUtils {
@@ -21,32 +27,30 @@ public class FileUtils {
 
   private FileUtils() {}
 
-  /**
-   * Copies all files and directories recursively from <code>source</code> to <code>target</code>,
-   * except paths that are given as <code>exclusions</code>.
-   *
-   * @param source Source path
-   * @param target Target path
-   * @param exclusions Excluded files or directories
-   * @throws IOException
-   */
-  public static void copyRecursively(final Path source, final Path target, final Path... exclusions)
+  public static List<Path> getAllPathsRecursively(
+      final Path source,
+      final boolean includeDirectories,
+      final Set<FileVisitOption> options,
+      final Path... exclusions)
       throws IOException {
-    final var exclusionPaths = Set.copyOf(Arrays.asList(exclusions));
+    if (!Files.exists(source)) {
+      return Collections.emptyList();
+    }
+    final var excludedSet = Set.of(exclusions);
+    final var files = new ArrayList<Path>();
     Files.walkFileTree(
         source,
+        options,
+        Integer.MAX_VALUE,
         new SimpleFileVisitor<>() {
           @Override
           public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
               throws IOException {
-            if (exclusionPaths.contains(dir)) {
-              LOG.trace("Directory {} is in list of exclusions, skipping", dir);
+            if (excludedSet.contains(dir)) {
               return FileVisitResult.SKIP_SUBTREE;
             }
-            final var targetDir = target.resolve(source.relativize(dir));
-            if (!Files.exists(targetDir)) {
-              LOG.trace("Creating directory source {} target {}", dir, targetDir);
-              Files.createDirectory(targetDir);
+            if (includeDirectories) {
+              files.add(dir);
             }
             return FileVisitResult.CONTINUE;
           }
@@ -54,54 +58,62 @@ public class FileUtils {
           @Override
           public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
               throws IOException {
-            if (!exclusionPaths.contains(file)) {
-              final var targetFile = target.resolve(source.relativize(file));
-              LOG.trace("Copying file {} target {}", file, targetFile);
-              Files.copy(file, targetFile);
-            } else {
-              LOG.trace("File {} is in list of exclusions, skipping", file);
+            if (!excludedSet.contains(file)) {
+              files.add(file);
             }
             return FileVisitResult.CONTINUE;
           }
         });
+    return files;
   }
 
-  /**
-   * Searches the given <code>searchDir</code> for files named like one of the given <code>fileNames
-   * </code> and returns their paths mapped to the filename.
-   *
-   * @param searchDir Directory to search in
-   * @param fileNames Filenames to look for
-   * @return Matching file-paths mapped against the lookup filename
-   * @throws IOException
-   */
+  public static void deleteRecursively(
+      final Path folder, final Set<FileVisitOption> options, final Path... exclusions)
+      throws IOException {
+    final var filesToDelete = getAllPathsRecursively(folder, true, options, exclusions);
+    Collections.reverse(filesToDelete); // will put the nested elements first
+    for (Path path : filesToDelete) {
+      Files.deleteIfExists(path);
+    }
+  }
+
+  public static void copyRecursively(
+      final Path source,
+      final Path target,
+      final Set<FileVisitOption> options,
+      final Path... exclusions)
+      throws IOException {
+    for (var sourcePath : getAllPathsRecursively(source, true, options, exclusions)) {
+      var copyToPath = target.resolve(source.relativize(sourcePath));
+      Files.copy(sourcePath, copyToPath);
+    }
+  }
+
   public static MultiValuedMap<String, Path> searchFileNamesRecursively(
-      final Path searchDir, Collection<String> fileNames) throws IOException {
+      final Path searchDir,
+      final Collection<String> fileNames,
+      final Set<FileVisitOption> options,
+      final Path... exclusions)
+      throws IOException {
     final var searchNames = Set.copyOf(fileNames);
     final var matches = new HashSetValuedHashMap<String, Path>();
-    if (!Files.exists(searchDir)) {
-        LOG.trace("Skipping saerch for file-names '{}', as directory {} does not exist", fileNames, searchDir);
-        return matches;
-    }
-    LOG.trace("Looking for file-names '{}' in folder {}...", fileNames, searchNames);
-    Files.walkFileTree(
-        searchDir,
-        new SimpleFileVisitor<>() {
-          @Override
-          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-            final var fileName = file.getFileName().toString();
-            if (searchNames.contains(fileName)) {
-              LOG.trace("Found matching file at {}", file);
-              matches.put(fileName, file);
-            }
-            return FileVisitResult.CONTINUE;
-          }
-        });
+
+    getAllPathsRecursively(searchDir, false, options, exclusions).stream()
+        .filter(file -> searchNames.contains(file.getFileName().toString()))
+        .forEach(match -> matches.put(match.getFileName().toString(), match));
     return matches;
   }
 
   public static String extractFileNameFromPath(final String path) {
-      var idx = path.lastIndexOf('/');
-      return idx > 0 ? path.substring(idx + 1) : path;
+    var idx = path.lastIndexOf('/');
+    return idx > 0 ? path.substring(idx + 1) : path;
+  }
+
+  public static void downloadFile(URL sourceUrl, Path targetFile) throws IOException {
+    try (var downloadStream = Channels.newChannel(sourceUrl.openStream());
+        var downloadTargetStream = new FileOutputStream(targetFile.toFile()).getChannel()) {
+      LOG.trace("Downloading file from {} to {}...", sourceUrl, targetFile);
+      downloadTargetStream.transferFrom(downloadStream, 0, Long.MAX_VALUE);
+    }
   }
 }
