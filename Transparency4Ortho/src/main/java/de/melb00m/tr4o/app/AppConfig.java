b7227d5bdad49ba9f1cdb14eb33fbe8fa70b8581
Martin Buchheim <martin.buchheim@ikor.de>
2020-05-02 23:01:34 +0200

    * Externalized configs * Updated XPToolsInterface * Overall improved code

---
+++ Transparency4Ortho/src/main/java/de/melb00m/tr4o/app/AppConfig.java
@@ -0,0 +1,106 @@
+package de.melb00m.tr4o.app;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.lang3.Validate;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public final class AppConfig {
+
+  private static AppConfig INSTANCE;
+  private final Config config;
+  private final RunArguments arguments;
+  private final CommandLine commandLine;
+
+  private AppConfig(final CommandLine commandLine) {
+    this.config = ConfigFactory.defaultApplication().resolve();
+    this.commandLine = commandLine;
+    this.arguments = readAndVerifyArguments(commandLine);
+    configureLoggingOutput(arguments.getConsoleLogLevel());
+  }
+
+  static void initialize(final CommandLine cmdLine) {
+    synchronized (AppConfig.class) {
+      if (null != INSTANCE) {
+        throw new IllegalStateException("AppConfig singleton was already initialized");
+      }
+      INSTANCE = new AppConfig(cmdLine);
+    }
+  }
+
+  public static Config getApplicationConfig() {
+    return INSTANCE.config;
+  }
+
+  public static RunArguments getRunArguments() {
+    return INSTANCE.arguments;
+  }
+
+  private RunArguments readAndVerifyArguments(final CommandLine line) {
+    final var args = line.getArgList();
+    Validate.isTrue(args.size() >= 3, "Expecting at least 3 path-parameters with application call");
+
+    final var xPlanePath = Path.of(args.get(0));
+    final var overlayPath = Path.of(args.get(1));
+    final var tilesPath =
+        args.subList(2, args.size()).stream()
+            .map(Paths::get)
+            .collect(Collectors.toUnmodifiableSet());
+    final var dsfToolExec = getOptionalPath("dx");
+
+    Validate.isTrue(
+        Files.isReadable(xPlanePath), "X-Plane location '%s' is not readable", xPlanePath);
+    Validate.isTrue(
+        Files.exists(overlayPath.resolve("Earth nav data")),
+        "Overlay path '%s' does not contain an expected 'Earth nav data' folder",
+        overlayPath);
+    tilesPath.forEach(
+        tile ->
+            Validate.isTrue(Files.isReadable(tile), "Tile directory '%s' is not readable", tile));
+    dsfToolExec.ifPresent(
+        dx -> Validate.isTrue(Files.isExecutable(dx), "DSFTool at '%s' is not executable", dx));
+
+    var logLevel = Level.INFO;
+    if (line.hasOption("d")) logLevel = Level.DEBUG;
+    if (line.hasOption("dd")) logLevel = Level.TRACE;
+
+    return new RunArguments(
+        xPlanePath,
+        tilesPath,
+        overlayPath,
+        getOptionalPath("dx"),
+        getOptionalPath("b"),
+        Optional.ofNullable(line.getOptionValue("lf")),
+        Optional.ofNullable(line.getOptionValue("lp")),
+        logLevel);
+  }
+
+  private Optional<Path> getOptionalPath(String config) {
+    return commandLine.hasOption(config)
+        ? Optional.of(Path.of(commandLine.getOptionValue(config)))
+        : Optional.empty();
+  }
+
+  private void configureLoggingOutput(final Level level) {
+    if (level != Level.INFO) {
+      Configurator.setRootLevel(level);
+      // for the console-appender, we need to modify the threshold filter to the new log-level
+      final var context = LoggerContext.getContext(false);
+      context.getRootLogger().getAppenders().values().stream()
+          .filter(ConsoleAppender.class::isInstance)
+          .map(ConsoleAppender.class::cast)
+          .forEach(appender -> appender.removeFilter(appender.getFilter()));
+      context.updateLoggers();
+    }
+  }
+}
---
+++ Transparency4Ortho/src/main/java/de/melb00m/tr4o/helper/FileHelper.java
@@ -0,0 +1,114 @@
+package de.melb00m.tr4o.helper;
+
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public final class FileHelper {
+
+  public static final Set<FileVisitOption> FOLLOW_SYMLINKS =
+      Collections.singleton(FileVisitOption.FOLLOW_LINKS);
+  private static final Logger LOG = LogManager.getLogger(FileHelper.class);
+
+  private FileHelper() {}
+
+  public static List<Path> getAllPathsRecursively(
+      final Path source,
+      final boolean includeDirectories,
+      final Set<FileVisitOption> options,
+      final Path... exclusions) {
+    if (!Files.exists(source)) {
+      return Collections.emptyList();
+    }
+    final var collector = new ExclusionAwareFileCollector(Set.of(exclusions), includeDirectories);
+    try {
+      Files.walkFileTree(source, options, Integer.MAX_VALUE, collector);
+      return collector.getCollectedFiles();
+    } catch (IOException ex) {
+      throw Exceptions.uncheck(ex);
+    }
+  }
+
+  public static void deleteRecursively(
+      final Path folder, final Set<FileVisitOption> options, final Path... exclusions) {
+    try {
+      final var filesToDelete = getAllPathsRecursively(folder, true, options, exclusions);
+      Collections.reverse(filesToDelete); // will put the nested elements first
+      for (Path path : filesToDelete) {
+        Files.deleteIfExists(path);
+      }
+    } catch (IOException ex) {
+      throw Exceptions.uncheck(ex);
+    }
+  }
+
+  public static void copyRecursively(
+      final Path source,
+      final Path target,
+      final Set<FileVisitOption> options,
+      final Path... exclusions) {
+    try {
+      for (var sourcePath : getAllPathsRecursively(source, true, options, exclusions)) {
+        var copyToPath = target.resolve(source.relativize(sourcePath));
+        Files.copy(sourcePath, copyToPath);
+      }
+    } catch (IOException ex) {
+      throw Exceptions.uncheck(ex);
+    }
+  }
+
+  public static MultiValuedMap<String, Path> searchFileNamesRecursively(
+      final Path searchDir,
+      final Collection<String> fileNames,
+      final Set<FileVisitOption> options,
+      final Path... exclusions) {
+    final var searchNames = Set.copyOf(fileNames);
+    final var matches = new HashSetValuedHashMap<String, Path>();
+
+    getAllPathsRecursively(searchDir, false, options, exclusions).stream()
+        .filter(file -> searchNames.contains(file.getFileName().toString()))
+        .forEach(match -> matches.put(match.getFileName().toString(), match));
+    return matches;
+  }
+
+  public static String extractFileNameFromPath(final String path) {
+    var idx = path.lastIndexOf('/');
+    return idx > 0 ? path.substring(idx + 1) : path;
+  }
+
+  public static void downloadFile(URL sourceUrl, Path targetFile) throws IOException {
+    try (var downloadStream = Channels.newChannel(sourceUrl.openStream());
+        var downloadTargetStream = new FileOutputStream(targetFile.toFile()).getChannel()) {
+      LOG.trace("Downloading file from {} to {}...", sourceUrl, targetFile);
+      downloadTargetStream.transferFrom(downloadStream, 0, Long.MAX_VALUE);
+    }
+  }
+
+  public static Path createAutoCleanedTempDir(final Path baseFolder, final Optional<String> prefix) {
+    try {
+      if (!Files.exists(baseFolder)) {
+        Files.createDirectories(baseFolder);
+      }
+      final var tempDir = Files.createTempDirectory(baseFolder, prefix.orElse(null));
+      Runtime.getRuntime()
+          .addShutdownHook(new Thread(() -> deleteRecursively(tempDir, Collections.emptySet())));
+      return tempDir;
+    } catch (IOException e) {
+      throw Exceptions.uncheck(e);
+    }
+  }
+}
